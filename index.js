718a58c0f41fd37d2b207faa25b14670c2357697
Felix Rilling <contact@f-rilling.com>
2017-05-11 23:53:42 +0200

    refactored main code

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 "use strict";
 
-const isDefined = val => typeof val !== "undefined";
-const isString = val => typeof val === "string";
-const getStartVal = function (val, arr) {
-    if (val === false) {
-        return 0;
-    } else {
-        return val < 0 ? arr.length + val : val;
-    }
-};
-const getEndVal = function (val, arr) {
-    if (val === false) {
-        return arr.length;
-    } else {
-        return val < 0 ? arr.length + val : val;
-    }
-};
+/**
+ * Converts negative indexes
+ * @param {Array} arr
+ * @param {Number} index
+ * @returns {Number}
+ */
+const calcIndex = (arr, index) => index < 0 ? arr.length + index : index;
+
+/**
+ * Returns an array with every n-th item
+ * @param {Array} arr
+ * @param {Number} step
+ * @returns {Array}
+ */
 const getStepped = (arr, step) => arr.filter((item, index) => index % step === 0);
 
 /**
@@ -26,18 +24,18 @@ const getStepped = (arr, step) => arr.filter((item, index) => index % step === 0
  * @param {Number}       step optional
  * @returns {String|Array}
  */
-module.exports = function (input, start, end, step) {
-    const inputIsString = isString(input);
+module.exports = function (input, start, end = null, step = null) {
+    const inputIsString = typeof input === "string";
     const arr = inputIsString ? input.split("") : input;
-    const startVal = getStartVal(start, arr);
+    const startVal = start === false ? 0 : calcIndex(arr, start);
 
-    if (isDefined(end)) {
-        const endVal = getEndVal(end, arr);
+    if (end !== null) {
+        const endVal = end === false ? arr.length : calcIndex(arr, end);
         let result;
 
         result = arr.slice(startVal, endVal);
 
-        if (isDefined(step)) {
+        if (step !== null) {
             if (step === 0) {
                 throw new RangeError("slice step cannot be zero");
             } else if (step < 0) {
